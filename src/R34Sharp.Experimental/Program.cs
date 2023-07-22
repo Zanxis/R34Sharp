e12bdd51ceb2f5320fc1253a61b838fe736dfaab
Starciad <davigamerlsf16@gmail.com>
2023-07-22 14:06:25 -0300

    feat: Addition of an experimental project that presents a basic structure of how to use the API.

---
+++ src/R34Sharp.Experimental/Program.cs
@@ -0,0 +1,38 @@
+﻿using R34Sharp;
+
+namespace R34Sharp.Experimental
+{
+    internal static class Program
+    {
+        private static string AssetsDirectory => Path.Combine(Directory.GetCurrentDirectory(), "R34Assets");
+
+        private static R34ApiClient _client = new();
+
+        [MTAThread]
+        private static async Task Main() {
+
+            if (!Directory.Exists(AssetsDirectory))
+            {
+                Directory.CreateDirectory(AssetsDirectory);
+            }
+
+
+            R34PostsSearchBuilder searchBuilder = new() {
+                Limit = 100,
+                Tags = new R34TagModel[]
+                {
+                    new("Bara"),
+                },
+            };
+
+            Console.Clear();
+
+            int count = 0;
+            foreach (R34Post post in (await _client.Posts.GetPostsAsync(searchBuilder)).Data)
+            {
+                Console.WriteLine($"File #{count}: {post.FileName}{post.FileExtension}");
+                count++;
+            }
+        }
+    }
+}
